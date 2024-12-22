3a275d6fd71b8d641975c33cf058d655ecf2922c
増田秀人 <smasuda@masudahidetonoMacBook-Air.local>
2024-12-22 22:57:31 +0900

    ファイルシステム

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,30 @@ mod kani_test;
 mod visualize_f32;
 //mod q7_format;
 mod cpu;
+mod pointer_intro;
+//mod check_heapCost;
+mod files;
 
 fn main() {
-    cpu::main();
+    // section5();
+    // cpu::main();
+    
+    // section6();
+    // section7();
+
+}
+
+fn section7(){
+    //files::serialize();
+    //files::hexdump();
+    files::writebyte_123();
+}
+
+fn section6(){
+    pointer_intro::main();
+    pointer_intro::rawPointer_cast();
+    pointer_intro::release_heapMemory();
+    //check_heapCost::main();
 }
 
 fn section5(){
