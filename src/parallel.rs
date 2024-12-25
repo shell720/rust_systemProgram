b9ebe0d2cff24e3782fdc058d72c8329e525d2b3
増田秀人 <smasuda@masudahidetonoMacBook-Air.local>
2024-12-26 08:24:21 +0900

    add

---
+++ src/parallel.rs
@@ -0,0 +1,73 @@
+use std::{thread, time};
+
+fn add (a: i32, b: i32) -> i32{
+    a+b
+}
+
+pub fn two_threads(){
+    let start = time::Instant::now();
+
+    let handler_1 = thread::spawn( || {
+        let pause = time::Duration::from_millis(500);
+        thread::sleep(pause.clone());
+    });
+
+    let handler_2 = thread::spawn(||{
+        let pause = time::Duration::from_millis(300);
+        thread::sleep(pause.clone());
+    });
+
+    handler_1.join().unwrap();
+    handler_2.join().unwrap();
+
+    let finish = time::Instant::now();
+
+    println!{"{:02?}", finish.duration_since(start)};
+}
+
+pub fn many_threads_sleep(){
+    for n in 1..=1000{
+        let mut handlers:Vec<thread::JoinHandle<()>> = Vec::with_capacity(n);
+
+        let start = time::Instant::now();
+        for _m in 0..n{
+            let handle = thread::spawn(||{
+                let pause = time::Duration::from_millis(20);
+                thread::sleep(pause);
+            });
+            handlers.push(handle);
+        }
+
+        while let Some(handle) = handlers.pop(){
+            handle.join();
+        }
+
+        let finish = time::Instant::now();
+        println!{"{}\t{:02?}", n, finish.duration_since(start)};
+    }
+}
+
+pub fn many_threads_busyweight(){
+    for n in 1..=1000{
+        let mut handlers:Vec<thread::JoinHandle<()>> = Vec::with_capacity(n);
+
+        let start = time::Instant::now();
+        for _m in 0..n{
+            let handle = thread::spawn(||{
+                let start = time::Instant::now();
+                let pause = time::Duration::from_millis(20);
+                while start.elapsed() < pause{
+                    thread::yield_now();
+                }
+            });
+            handlers.push(handle);
+        }
+
+        while let Some(handle) = handlers.pop(){
+            handle.join();
+        }
+
+        let finish = time::Instant::now();
+        println!{"{}\t{:02?}", n, finish.duration_since(start)};
+    }
+}
